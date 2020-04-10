9491f3f82cc0b3222f656e395bf79ff43c0c9c49
trini7y <okekedesmond71@gmail.com>
2020-04-10 09:23:07 +0100

    Fixed most error and added some feature

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 let createBtn  = document.getElementById("btn");
+let navigate = document.getElementById("nav");
+let message  = document.getElementById("welcome");
 let date = new Date();
 
-createBtn.onclick = function(e){
+
+navigate.onclick = (e) => {
+	let user_name = ( ( document.getElementById("userName") || {} ).value) || "";
+	if(user_name == ""){
+		alert("You must enter your name");
+	}
+	else{
+		console.log(user_name);
+		message.createTextNode("Welcome {{user_name}}");
+		
+	}
+	
+createBtn.onclick = (e) => {
  	let input = ( (document.getElementById("inputVal") || {} ).value) || "";
 	let todos = document.createElement("li");
 	let parag = document.createElement("p").value="\u9742";
@@ -15,4 +29,7 @@ createBtn.onclick = function(e){
 	}
 	input.value = "";
 	console.log(input);
+}
+
+
 }
