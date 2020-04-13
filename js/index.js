f9fd70ce12907d1dbc0e4d81c1a66db9bc2ae921
trini7y <okekedesmond71@gmail.com>
2020-04-14 00:53:02 +0100

    added the manifest.json file

--- js/index.js
+++ js/index.js
@@ -2,34 +2,22 @@ let createBtn  = document.getElementById("btn");
 let navigate = document.getElementById("nav");
 let message  = document.getElementById("welcome");
 let date = new Date();
-
-
-navigate.onclick = (e) => {
-	let user_name = ( ( document.getElementById("userName") || {} ).value) || "";
-	if(user_name == ""){
-		alert("You must enter your name");
-	}
-	else{
-		console.log(user_name);
-		message.createTextNode("Welcome {{user_name}}");
 		
-	}
 	
 createBtn.onclick = (e) => {
  	let input = ( (document.getElementById("inputVal") || {} ).value) || "";
 	let todos = document.createElement("li");
-	let parag = document.createElement("p").value="\u9742";
-	todos.appendChild(document.createTextNode(parag + input + date) );
+	let todoDate = document.createElement("p").value = date;
+	let parag = document.createElement("p").value="";
+	todos.setAttribute("class", "lists");
+	//todoDate.setAttribute();
+	todos.appendChild(document.createTextNode(parag + input +"<br>"+ todoDate ) );
 	if(input != ""){
 		document.getElementById("uList").appendChild(todos);
-		todos.style.border.bottom = "1px solid #0A0503";
+		input = "";
 	}
 	else{
 		alert("You have not inputed any value");
 	}
-	input.value = "";
 	console.log(input);
-}
-
-
 }
